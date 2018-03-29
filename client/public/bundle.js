3e99e77b76ac78f266b06903da802c5e4a8ad63b
abbasnake <mr.rihards.ozols@gmail.com>
2018-03-29 19:18:36 +0300

    refactored the registry js

--- client/public/bundle.js
+++ client/public/bundle.js
@@ -685,76 +685,28 @@
   };
 
   const runRegistry = () => {
-    // const dateContainer      = $('.js-date')
-    // const aScoreContainer    = $('.js-A-score')
-    // const rScoreContainer    = $('.js-R-score')
-    // const aCommentsContainer = $('.js-A-comments')
-    // const rCommentsContainer = $('.js-R-comments')
-
-    // const insertDate = (index) => {
-    //   const currentDate = $$$('li')
-    //   currentDate.textContent = `${db.schedule[index].date}`
-    //   dateContainer.appendChild(currentDate)
-    // }
-
-    // const insertDataA = (index) => {
-    //   const currentScore = $$$('li')
-    //   currentScore.textContent = `${db.schedule[index].score.A}`
-    //   aScoreContainer.appendChild(currentScore)
-    //   const currentComment = $$$('li')
-    //   currentComment.textContent = `${db.schedule[index].comments.A}`
-    //   aCommentsContainer.appendChild(currentComment)
-    // }
-
-    // const insertDataR = (index) => {
-    //   const currentScore = $$$('li')
-    //   currentScore.textContent = `${db.schedule[index].score.R}`
-    //   rScoreContainer.appendChild(currentScore)
-    //   const currentComment = $$$('li')
-    //   currentComment.textContent = `${db.schedule[index].comments.R}`
-    //   rCommentsContainer.appendChild(currentComment)
-    // }
-
-    // const insertData = () => {
-    //   for (let i = 0; i < db.schedule.length; i++) {
-    //     insertDate(i)
-    //     insertDataA(i)
-    //     insertDataR(i)
-    //   }
-    // }
-
-    // insertData()
 
     const container = $('.js-registry');
 
+    const makeLi = (data, parent) => {
+      const li = $$$('li');
+      li.textContent = data;
+      parent.appendChild(li);
+    };
+
     const insertData = () => {
-      for (let i = 0; i < db.schedule.length; i++) {
-        // create ul
-        const ul = $$$('ul');
-        ul.classList.add('l-table-list');
-        //add li's
-        const liDate = $$$('li');
-        liDate.textContent = db.schedule[i].date;
-        ul.appendChild(liDate);
+      for (let i = 0; i < db.schedule.length; i++) { // loop through db
+        
+        const ul = $$$('ul'); // create ul to place data li's in
+        ul.classList.add('l-table-list'); // add SMACSS layout class
 
-        const liA = $$$('li');
-        liA.textContent = db.schedule[i].score.A;
-        ul.appendChild(liA);
+        makeLi(db.schedule[i].date, ul); //add li's with data to ul
+        makeLi(db.schedule[i].score.A, ul);
+        makeLi(db.schedule[i].score.R, ul);
+        makeLi(db.schedule[i].comments.A, ul);
+        makeLi(db.schedule[i].comments.R, ul);
 
-        const liR = $$$('li');
-        liR.textContent = db.schedule[i].score.R;
-        ul.appendChild(liR);
-
-        const liCommentA = $$$('li');
-        liCommentA.textContent = db.schedule[i].comments.A;
-        ul.appendChild(liCommentA);
-
-        const liCommentR = $$$('li');
-        liCommentR.textContent = db.schedule[i].comments.R;
-        ul.appendChild(liCommentR);
-
-        //add ul to container
-        container.appendChild(ul);
+        container.appendChild(ul); //add ul to container
       }
     };
 
